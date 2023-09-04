78077e5fe66eaaa8d7d41d7902939261a032e646
anna2023471 <russiaanna@yahoo.co.nz>
2023-09-04 22:18:43 +1200

    panel code complete

--- static/js/app.js
+++ static/js/app.js
@@ -25,11 +25,6 @@ d3.json(url).then(function(data) {
     const selectedMeta = demographics.find(demographics => demographics.id == selectedID);
     console.log(selectedMeta);
 
-    // let metaKeys = Object.keys(selectedMeta);
-    // console.log(metaKeys);
-    // let metaValues = Object.values(selectedMeta);
-    // console.log(metaValues);
-
     let id = selectedMeta.id;
     let ethnicity = selectedMeta.ethnicity;
     let gender = selectedMeta.gender;
@@ -37,24 +32,15 @@ d3.json(url).then(function(data) {
     let location = selectedMeta.location;
     let bbtype = selectedMeta.bbtype;
     let wfreq = selectedMeta.wfreq;
-
-    // d3.select(".panel-body").text(`${metaKeys}: ${metaValues}`);
   
-    d3.select(".panel-body").text(`id: ${id}\nethnicity: ${ethnicity}\ngender: ${gender}\nage: ${age}\nlocation: ${location}\nbbtype: ${bbtype}\nwfreq: ${wfreq}`);
+    d3.select(".panel-body").html(`id: ${id} <br> ethnicity: ${ethnicity} <br> gender: ${gender} <br> age: ${age} <br> location: ${location} <br> bbtype: ${bbtype} <br> wfreq: ${wfreq}`);
 
-    // let list = panel.append("ul");
-    // list.append("li").text(`id: ${id}`);
-    // list.append("li").text(`ethnicity: ${ethnicity}`);
-    // list.append("li").text(`gender: ${gender}`);
-    // list.append("li").text(`age: ${age}`);
-    // list.append("li").text(`location: ${location}`);
-    // list.append("li").text(`bbtype: ${bbtype}`);
-    // list.append("li").text(`wfreq: ${wfreq}`);
     console.log(selectedData.sample_values);
+    
 
     const trace1 = {
       x: Object.values(selectedData.sample_values).slice(0,10),
-      y: Object.values(selectedData.otu_ids),
+      y: Object.values(selectedData.otu_ids).map(String),
       text: Object.values(selectedData.otu_labels),
       type: "bar",
       orientation: "h"
@@ -72,7 +58,7 @@ d3.json(url).then(function(data) {
     let marker_colour = Object.values(selectedData.otu_ids);
 
     const trace2 = {
-        x: Object.values(selectedData.otu_ids),
+        x: Object.values(selectedData.otu_ids).join().split(','),
         y: Object.values(selectedData.sample_values).slice(0,10),
         text: Object.values(selectedData.otu_labels),
         mode: "markers",
