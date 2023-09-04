5af691060011aba362cf52b5abd8d571c98ca76d
anna2023471 <russiaanna@yahoo.co.nz>
2023-09-04 22:37:02 +1200

    minor console.log delete

--- static/js/app.js
+++ static/js/app.js
@@ -34,9 +34,7 @@ d3.json(url).then(function(data) {
     let wfreq = selectedMeta.wfreq;
   
     d3.select(".panel-body").html(`id: ${id} <br> ethnicity: ${ethnicity} <br> gender: ${gender} <br> age: ${age} <br> location: ${location} <br> bbtype: ${bbtype} <br> wfreq: ${wfreq}`);
-
-    console.log(selectedData.sample_values);
-    
+ 
 
     const trace1 = {
       x: Object.values(selectedData.sample_values).slice(0,10),
